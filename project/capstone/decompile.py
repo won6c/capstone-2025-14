243b2ff21d06bf541eb8ae7d9115e8b96807604b
won6c <superken1227@kookmin.ac.kr>
2025-03-23 23:03:31 +0900

    edit path

--- project/capstone/decompile.py
+++ project/capstone/decompile.py
@@ -1,15 +1,18 @@
 import os
 import subprocess
 from django.http import JsonResponse
+from django.conf import settings
 
 def decompile(request):
     if request.method == "POST" and request.FILES.get("file"):
         uploaded_file = request.FILES["file"]
-        file_path = os.path.join("media/uploads", uploaded_file.name)
+        uploads_dir = os.path.join(settings.MEDIA_ROOT,"uploads")
+        file_path = os.path.join(uploads_dir, uploaded_file.name)
 
         # media/uploads 폴더 생성
-        if not os.path.exists("media/uploads"):
-            os.makedirs("media/uploads")
+        if not os.path.exists(uploads_dir):
+            os.makedirs(uploads_dir,exist_ok=True)
+            os.makedirs(os.path.join(settings.MEDIA_ROOT,"downloads"),exist_ok=True)
 
         # 업로드된 파일 저장
         with open(file_path, "wb+") as destination:
@@ -29,9 +32,9 @@ def decompile(request):
     return JsonResponse({"error": "Invalid request"}, status=400)
 
 def run_decompiler(file_path):
-    decompiled_output = f"{file_path}.c"
+    decompiled_output = os.path.join(settings.MEDIA_ROOT,"downloads",f"{os.path.basename(file_path)}.c")
     # 예시: bof.c 파일을 복사하여 디컴파일 결과 파일 생성
-    command = f"cp /app/project/capstone/llm4decompile/bof.c {decompiled_output}"
+    command = f"cp /app/llm4decompile/bof.c {decompiled_output}"
     # 명령어 실행 (check=True: 실패 시 예외 발생)
     subprocess.run(command, shell=True, check=True)
     
